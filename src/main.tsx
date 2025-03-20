11a1af3086766c42bb330925f81062b6e82589eb
mohamed <hackkhante@gmail.com>
2025-03-20 23:57:28 +0200

    Fix line endings

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/TO_DO_App">
       <App />
     </BrowserRouter>
   </React.StrictMode>
